4e31c9ee6d16bd3b8422a67f6ca34c0a3b6eb53e
saaid111 <saaidmohamed603@gmail.com>
2025-03-13 20:41:55 +0000

    revise

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,10 @@
 #!/usr/bin/env bash
 
-# Ensure that the system is updated and the necessary packages are installed
 sudo apt update && sudo apt install -y nodejs npm
-
-# Install PM2 globally
 sudo npm install -g pm2
-
-# Stop any existing process of SimpleApp
 pm2 stop SimpleApp
-
-# Copy the private and server keys from the ubuntu user's home directory
 cp /home/ubuntu/privatekey.pem SimpleApplication/privatekey.pem
 cp /home/ubuntu/server.crt SimpleApplication/server.crt
-
-# Navigate to the application directory
 cd SimpleApplication/
-
-# Install the required dependencies for the application
 npm install
-
-# Start the application using PM2
 pm2 start ./bin/www --name SimpleApp
