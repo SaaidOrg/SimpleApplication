49ebd0a48dc3ae5d77fef78206a54ec3c0c88230
saaid111 <saaidmohamed603@gmail.com>
2025-03-14 21:03:44 +0000

    new

--- deploy.sh
+++ deploy.sh
@@ -1,27 +1,11 @@
 #!/usr/bin/env bash
-#!/usr/bin/env bash
 
-
-sudo apt update && sudo apt install -y nodejs npm git
-
-
+sudo apt update && sudo apt install -y nodejs npm
 sudo npm install -g pm2
-
 pm2 stop SimpleApp
-
-
-if [ ! -d "SimpleApplication" ]; then
-    git clone https://github.com/SaaidOrg/SimpleApplication.git SimpleApplication
-fi
-
 cp /home/ubuntu/privatekey.pem SimpleApplication/privatekey.pem
 cp /home/ubuntu/server.crt SimpleApplication/server.crt
-
 cd SimpleApplication/
-
 npm install
-
-chmod 644 privatekey.pem
-chmod 644 server.crt
 
 pm2 start ./bin/www --name SimpleApp
