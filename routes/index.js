014272825e5ad7e87cbbce833f8123376127495b
saaid111 <saaidmohamed603@gmail.com>
2025-03-14 20:47:50 +0000

    revise

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: "Simple Application version 2" });
-}); 
+});   
 
 
 module.exports = router;
